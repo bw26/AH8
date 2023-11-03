2536f7c8cd5eccd6e59195be1bbbae3b0df0828a
Wu <brian.wu@fiserv.com>
2023-11-03 13:32:45 -0400

    AH8: Optimized

--- ECSource.java
+++ ECSource.java
@@ -1,14 +1,23 @@
 class ECSource {
   public static <T> T getNodeValue(Node<T> head, int index) {
     // todo - note the return type, but don't overthink it
-    int curr = 0;
-    while (head != null) {
-      if (curr == index)
-        return head.val;
-      curr++;
+
+    //Method 1:
+    // int curr = 0;
+    // while (head != null) {
+    //   if (curr == index)
+    //     return head.val;
+    //   curr++;
+    //   head = head.next;
+    // }
+    // return null;
+
+    //Method 2
+    for(int i = 0; i < index; i++){
+      if (head == null) return null;
       head = head.next;
     }
-    return null;
+    return head.val;
   }
 
   public static void main(String[] args) {
@@ -21,7 +30,7 @@ class ECSource {
 
     // banana -> mango -> kiwi
 
-    System.out.println(ECSource.getNodeValue(node1, 1));
+    System.out.println(ECSource.getNodeValue(node1, 4)); //mango
     System.out.println(ECSource.getNodeValue(node1, 0)); // "banana"
 
     Node<String> a = new Node<>("a");
